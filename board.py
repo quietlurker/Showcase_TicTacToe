c6a485fa4c8e8b10674807c35c34c0eb328ccdb3
quiet_lurker <ghosia.spam@gmail.com>
2023-09-27 18:42:56 +0200

    Add dynamic board drawing and final polish

--- board.py
+++ board.py
@@ -1,23 +1,55 @@
-# TODO add docstrings
 class TicTacToeBoard:
     def __init__(self, board_size):
         self.board_size = board_size
         # without list_name: list = [] pycharm shows warnings if non-int values are put in the list later in the code
-        self.board_state: list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.board_state: list = []
+        self.create_board()
         self.print_board()
         self.current_player = "x"  # "o"
         self.game_over = False
         self.winner = None
 
+    def create_board(self):
+        list_value = 1
+        for _ in range(self.board_size ** 2):
+            self.board_state.append(list_value)
+            list_value += 1
+
     def print_board(self):
-        # TODO: draw board with variable size...
-        print(f" {self.board_state[0]} | {self.board_state[1]} | {self.board_state[2]}\n"
-              "----------\n"
-              f" {self.board_state[3]} | {self.board_state[4]} | {self.board_state[5]}\n"
-              "----------\n"
-              f" {self.board_state[6]} | {self.board_state[7]} | {self.board_state[8]}\n")
+        board = ""
+        init_value = 1
+        for row in range(self.board_size):
+            for column in range(self.board_size):
+
+                # space characters properly - one space if 2char value, two spaces for 1 char values
+                if self.board_state[init_value - 1] in ("x", "o", 1, 2, 3, 4, 5, 6, 7, 8, 9):
+                    board += f"  {self.board_state[init_value - 1]}  "
+                else:
+                    board += f" {self.board_state[init_value - 1]}  "
+
+                # add line end at the last column
+                if column == self.board_size - 1:
+                    board += "\n"
+                else:
+                    board += "|"
+                init_value += 1
+
+            # don't add row separator for last row
+            if row != self.board_size - 1:
+                # separator size is 6*"-" per column except last one which is 5*"-" so replace it with line end
+                for _ in range(self.board_size):
+                    board += "------"
+                board = board[:-1] + "\n"
+
+        print(board)
 
     def update_board(self, player_input):
+        """
+        Updates board with player's choice.
+        Checks victory conditions via check_board() and check_if_moves_left() methods.
+        Changes player if game continues.
+        Prints current board.
+        """
         if player_input in self.board_state:
             self.board_state[player_input - 1] = self.current_player
             self.check_board()
@@ -30,20 +62,31 @@ class TicTacToeBoard:
             self.print_board()
 
     def check_if_moves_left(self):
-        # check if no more fields
+        """
+        Checks if no more fields left.
+        Sets game_over attribute when no more moves left.
+        """
         nb_of_x = self.board_state.count("x")
         nb_of_o = self.board_state.count("o")
         if nb_of_x + nb_of_o == self.board_size * self.board_size:
             self.game_over = True
 
     def check_win(self, list_to_check):
+        """
+        Helper to check_board.
+        Input: list with row or column or diagonal.
+        sets winner and game_over if player won.
+        """
         if list_to_check.count("x") == self.board_size or list_to_check.count("o") == self.board_size:
             self.winner = self.current_player
             self.game_over = True
 
     def check_board(self):
+        """
+        Check victory conditions for rows, columns and diagonals.
+        Sets winner and game_over attributes via check_win() method.
+        """
         # check if anyone won
-
         # rows
         row_start_pos = 0
         row_count = 1
@@ -91,4 +134,3 @@ class TicTacToeBoard:
                         start_pos = start_pos + self.board_size - 1
                         # right to left
                         step = self.board_size - 1
-
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tictactoe_art import logo
 from board import TicTacToeBoard
 
+
 def welcome_message():
     start_message = ("Select game mode:\n"
                      "3: Classic (3x3)\n"
@@ -11,46 +12,55 @@ def welcome_message():
     return input("Board size: ")
 
 
-board_size_int = ""
+def play_game():
+    init_selection = False
+    board_size_int = ""
+    game_modes = [0, 3, 4, 5, ]
+
+    # game init
+    while not init_selection:
+        try:
+            board_size_int = int(welcome_message())
+        except ValueError:
+            print("Select valid option")
+        else:
+            if board_size_int in game_modes:
+                init_selection = True
+            else:
+                print("Select valid option")
+
+    if board_size_int == 0:
+        print("OK, bye!")
+    else:
+        # play the game
+        game_on = True
+        board = TicTacToeBoard(board_size_int)
+        while game_on:
+            player_choice = input(
+                f"Player {board.current_player}: where do you want to place '{board.current_player}'? ")
+            try:
+                board.update_board(int(player_choice))
+            except ValueError:
+                print("Wrong answer")
+
+            # check if anyone one
+            if board.winner:
+                print(f"Game over! Player {board.current_player} wins!")
+                game_on = False
+            # check if there's still space on the board
+            else:
+                if board.game_over:
+                    game_on = False
+                    print("Game over! No more moves")
+            if not game_on:
+                if input("Do you want to play again? y/n ").lower == "y":
+                    play_game()
+                else:
+                    print("OK, bye!")
+
+
+# start game
 print(logo)
 print("Welcome to tic tac toe! \n")
-game_modes = [0, 3, 4, 5,]
 
-init_selection = False
-
-# game init
-while not init_selection:
-    try:
-        board_size_int = int(welcome_message())
-    except ValueError:
-        print("Select valid option")
-    else:
-        if board_size_int in game_modes:
-            init_selection = True
-        else:
-            print("Select valid option")
-
-
-if board_size_int == 0:
-    print("OK, bye!")
-else:
-    # play the game
-    game_on = True
-    board = TicTacToeBoard(board_size_int)
-    while game_on:
-        player_choice = input(f"Player {board.current_player}: where do you want to place '{board.current_player}'? ")
-        try:
-            board.update_board(int(player_choice))
-        except ValueError:
-            print("Wrong answer")
-
-        # check if anyone one
-        if board.winner:
-            print(f"Game over! Player {board.current_player} wins!")
-            game_on = False
-        # check if there's still space on the board
-        else:
-            if board.game_over:
-                game_on = False
-                print("Game over! No more moves")
-
+play_game()
